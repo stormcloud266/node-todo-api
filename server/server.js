13ed91d547ffd3c669c83aee945cff33d7e5592f
stormcloud266 <allprt89@gmail.com>
2018-06-12 15:20:53 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/Todo');
+const { User } = require('./models/User');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+
+// =============================================
+// posts new todo
+// =============================================
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// =============================================
+// gets all todos
+// =============================================
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos })
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+// =============================================
+// gets todo by id
+// =============================================
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
