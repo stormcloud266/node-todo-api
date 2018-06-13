b17d752c1a219f99846507b9ab6d618565f3bfbf
stormcloud266 <allprt89@gmail.com>
2018-06-13 11:53:42 -0600

    added get users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -116,6 +117,17 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// =============================================
+// private route
+// =============================================
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+});
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
