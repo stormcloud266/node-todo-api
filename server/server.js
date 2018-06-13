e9f1d7842dff8df47e8460d45804156993019c67
stormcloud266 <allprt89@gmail.com>
2018-06-13 11:12:00 -0600

    added token method

--- server/server.js
+++ server/server.js
@@ -101,6 +101,21 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// =============================================
+// creates new user
+// =============================================
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+  const user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
